4bbbf2240895651950998d97ee699b281eefbf58
harsh <harsh@HARSH-LAPTOP>
2025-09-20 00:14:35 +0530

    Add project files.

---
+++ TravelMemories.McpServer/Controllers/TestingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelMemories.McpServer.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class TestingController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> TestController()
+        {
+            return "Working Successfully";
+        }
+    }
+}
---
+++ TravelMemories.McpServer/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddCookie(x => x.Cookie.Name = "travelMemoriesToken")
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["IssuerSigningKeySecretText"])),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            context.Token = context.Request.Cookies["travelMemoriestoken"];
+            return Task.CompletedTask;
+        }
+    };
+});
+
+builder.Services.AddMcpServer().WithHttpTransport().WithToolsFromAssembly();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapMcp();
+//app.MapMcp().RequireAuthorization();
+
+app.Run();
