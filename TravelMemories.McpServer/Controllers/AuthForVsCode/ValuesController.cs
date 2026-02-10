98f1d9376230667e18a53c3228e5554fb24da34a
Harsh Jain <harshjain17may@gmail.com>
2026-02-10 22:09:20 +0530

    test html for verification

---
+++ TravelMemories.McpServer/Controllers/AuthForVsCode/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelMemories.McpServer.Controllers.AuthForVsCode
+{
+    [Route("authorize")]
+    [ApiController]
+    [AllowAnonymous]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task RenderPermissionPage(
+            [FromQuery] string client_id, 
+            [FromQuery] string response_ype,
+            [FromQuery] string code_challenge,
+            [FromQuery] string code_challenge_method,
+            [FromQuery] string scope,
+            [FromQuery] string redirect_uri,
+            [FromQuery] string state)
+        {
+            var html = @"
+<html>
+    <body>
+        please grant permission
+    </body>
+</html>
+
+            ";
+
+            HttpContext.Response.ContentType = "text/html";
+            await HttpContext.Response.WriteAsync(html);
+        }
+    }
+}
